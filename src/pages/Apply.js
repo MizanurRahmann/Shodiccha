8555171ddd31e1590a81eb3e88f54c6bf7187d78
MizanurRahmann <mr926560@gmail.com>
2021-01-03 19:26:58 +0600

    update question form, apply and enter page.

--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -61,6 +61,21 @@ function Apply() {
             <div className="apply__image-block">
                 <img src={volunteer} alt="" />
             </div>
+
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="180"
+                height="180"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-square square"
+            >
+                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+            </svg>
         </div>
     );
 }
