968d60a407153fa5cc96c9dc3f44e38f198ede8f
MizanurRahmann <mr926560@gmail.com>
2021-01-18 21:27:27 +0600

    remove side padding from about page and add it to its all components.

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,15 +6,17 @@ import Description from "../components/Description";
 import Summary from "../components/Summary";
 import PastActivities from "../components/PastActivities";
 import NavForViewer from "../components/NavForViewer";
+import ApplyForVolunteer from "../components/ApplyForVolunteer";
 
 function About() {
   return (
-    <div style={{ padding: "100px" }}>
+    <div style={{ padding: "100px 0" }}>
       <NavForViewer />
       <Banner />
       <Description />
       <Summary />
       <PastActivities />
+      <ApplyForVolunteer />
     </div>
   );
 }
