f452fbf7f26f4c003075c05c66f08f707e9837c6
MizanurRahmann <mr926560@gmail.com>
2021-01-18 20:46:08 +0600

    add: custom arrow button created for slider.

--- src/components/PastActivities.js
+++ src/components/PastActivities.js
@@ -17,6 +17,31 @@ function PastActivities() {
       <p>আমাদের</p>
       <h1>সফল উদ্যোগসমূহ</h1>
 
+      <div className="button_box">
+        <div className="round_btn">
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z" />
+          </svg>
+        </div>
+        <div className="round_btn">
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+          </svg>
+        </div>
+      </div>
+
       <div className="pastact__slider-block">
         <Slider {...settings} className="slider">
           <ActivityCard value={1} />
