f4dd50dbd4168f3a7760fab6d44f22403310df1e
MizanurRahmann <mr926560@gmail.com>
2021-01-20 18:06:05 +0600

    update members component.

---
+++ src/components/MemberCard.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import network from "../assets/images/network.svg";
+
+function MemberCard({ image, name, desegnition }) {
+  const [backgroundColor, setBackgroundColor] = useState("");
+  const [textColor, setTextColor] = useState("");
+
+  const checkDesegnition = (desegnition) => {
+    if (desegnition === "Director") {
+      setBackgroundColor("#FDDBEB");
+      setTextColor("#F27294");
+    } else if (desegnition === "Secretary") {
+      setBackgroundColor("#E7F2FA");
+      setTextColor("#94CEF2");
+    } else if (desegnition === "Editor") {
+      setBackgroundColor("#D4EFDC");
+      setTextColor("#76bf54");
+    } else {
+      setBackgroundColor("#FFF3D4");
+      setTextColor("#F2A649");
+    }
+  };
+
+  useEffect(() => {
+    checkDesegnition(desegnition);
+  }, []);
+
+  return (
+    <div className="member-card">
+      <img src={network} style={{ margin: "0 50px -60px 0" }} alt="" />
+      <img src={image} alt="" />
+
+      <div className="info">
+        <h2>{name}</h2>
+        <div
+          className="desegnition"
+          style={{ background: backgroundColor, color: textColor }}
+        >
+          <div className="round" style={{ background: textColor }}></div>
+          {desegnition}
+        </div>
+
+        <div className="social-links">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-activity"
+          >
+            <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
+          </svg>
+          <Link to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-facebook"
+            >
+              <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+            </svg>
+          </Link>
+
+          <Link to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-instagram"
+            >
+              <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MemberCard;
--- src/components/Members.js
+++ src/components/Members.js
@@ -1,14 +1,52 @@
 import React from "react";
 import "../styles/Members.scss";
 
+// Assets
+import flower from "../assets/images/flower-3.svg";
+import member1 from "../assets/images/member-2.png";
+import member2 from "../assets/images/member-3.png";
+import member3 from "../assets/images/member-4.png";
+
+// Components
+import MemberCard from "./MemberCard";
+
 function ApplyForVolunteer() {
   return (
-    <div className="applyForVolunteer">
+    <div className="members-block">
       <div className="flat__heading">
         <p>সদস্যবৃন্দ</p>
         <h1>
           আমাদের সম্মানিত সদস্যবৃন্দ <span></span>
         </h1>
+      </div>
+
+      <div className="flowers">
+        <div className="flower-left">
+          <img src={flower} alt="" />
+        </div>
+        <div className="flower-right">
+          <img src={flower} alt="" />
+        </div>
+      </div>
+
+      <div className="members" style={{ width: "60%", margin: "auto" }}>
+        <MemberCard
+          image={member1}
+          name="বারিক সাদিক"
+          desegnition="Secretary"
+        />
+
+        <MemberCard
+          image={member2}
+          name="মিজানুর রহমান"
+          desegnition="Director"
+        />
+
+        <MemberCard
+          image={member3}
+          name="সাব্বির হাসান রেজা"
+          desegnition="Editor"
+        />
       </div>
     </div>
   );
