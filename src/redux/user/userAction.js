769a0a9f813a054498c9333df5257f15a658f186
MizanurRahmann <mr926560@gmail.com>
2021-01-24 04:32:24 +0600

    save neccessary information and ignore others.

--- src/redux/user/userAction.js
+++ src/redux/user/userAction.js
@@ -3,7 +3,13 @@ import { SET_USER, CLEAR_USER } from "./userType";
 export const setUser = (currentUser) => {
   return {
     type: SET_USER,
-    currentUser: currentUser,
+    currentUser: {
+      uid: currentUser.uid,
+      email: currentUser.email,
+      displayName: currentUser.displayName,
+      photoURL: currentUser.photoURL,
+      emailVerified: currentUser.emailVerified,
+    },
   };
 };
 
