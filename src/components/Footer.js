1b29a07d34da68bc87a844655a93a38930d47da9
MizanurRahmann <mr926560@gmail.com>
2021-02-08 17:37:18 +0600

    make about page little bit responsive.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -65,7 +65,7 @@ function Footer() {
               <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
               <polyline points="22,6 12,13 2,6"></polyline>
             </svg>
-            <div>shodicchaBogura@gmail.com</div>
+            {/* <div>shodicchaBogura@gmail.com</div> */}
           </div>
 
           <div className="connection">
