8747fa78fe9614e21850f5352483141a3432b0c9
MizanurRahmann <mr926560@gmail.com>
2021-02-11 20:09:02 +0600

    responsive banner for mobile devices.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,14 +7,16 @@ function Banner() {
         <div className="banner">
             <div className="banner__heading">
                 <h1>
-                    <span>স্বদিচ্ছা</span> ফাউন্ডেশনে
+                    <span>স্বদিচ্ছা</span> ফাউন্ডেশন
                 </h1>
                 <h2>আপ্নাকে স্বাগতম</h2>
             </div>
-            <p>
-                সদিচ্ছা একটি স্বেচ্ছাসেবী সংস্থা। <span>মানবতা</span>,{" "}
-                <span>বন্ধন</span>, <span>সহযোগিতা</span> যার মূল নীতি।
-            </p>
+            <div className="banner__text">
+                <p>
+                    সদিচ্ছা একটি স্বেচ্ছাসেবী সংস্থা। <span>মানবতা</span>,{" "}
+                    <span>বন্ধন</span>, <span>সহযোগিতা</span> যার মূল নীতি।
+                </p>
+            </div>
             <img src={dataAnalysis} alt="" />
         </div>
     );
