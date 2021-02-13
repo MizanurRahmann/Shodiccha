5b3c172a5ab9b12344a228ef2988f7a811298946
MizanurRahmann <mr926560@gmail.com>
2021-02-14 00:31:04 +0600

    make recent-activity-cards responsive for mobile devices.

--- src/components/Activities/PastActivities.js
+++ src/components/Activities/PastActivities.js
@@ -12,6 +12,30 @@ function PastActivities() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   return (
