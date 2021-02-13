9f43acd3d4e89d62dca4b0806a807ec50e264842
MizanurRahmann <mr926560@gmail.com>
2021-02-13 19:11:40 +0600

    update: recent activities block.

--- src/components/Activities/RecentActivity.js
+++ src/components/Activities/RecentActivity.js
@@ -12,6 +12,31 @@ function RecentActivity() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   return (
