6a1b6d0020b8bd511745797f696b3cd1c3d0ae2f
MizanurRahmann <mr926560@gmail.com>
2021-02-20 19:11:26 +0600

    make responsive recent event component for mobile devices.

--- src/components/Activities/PastActivities.js
+++ src/components/Activities/PastActivities.js
@@ -18,24 +18,23 @@ function PastActivities() {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          
-        }
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-        }
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -43,34 +42,37 @@ function PastActivities() {
       <p>আমাদের</p>
       <h1>সফল উদ্যোগসমূহ</h1>
 
-      <div className="button_box">
-        <div
-          className="round_btn"
-          onClick={() => customSlider.current.slickPrev()}
-        >
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+      {/* Slider button */}
+      <div className="slider_btn">
+        <div className="button_box">
+          <div
+            className="round_btn"
+            onClick={() => customSlider.current.slickPrev()}
           >
-            <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z" />
-          </svg>
-        </div>
-        <div
-          className="round_btn"
-          onClick={() => customSlider.current.slickNext()}
-        >
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+              <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z" />
+            </svg>
+          </div>
+          <div
+            className="round_btn"
+            onClick={() => customSlider.current.slickNext()}
           >
-            <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
-          </svg>
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+              <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+            </svg>
+          </div>
         </div>
       </div>
 
